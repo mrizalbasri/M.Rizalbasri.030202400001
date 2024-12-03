6caf0a91b10a856dc21f509a8994cf7d5e3fbe82
mrizalbasri <m.basri@student.president.ac.id>
2024-12-03 04:13:30 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,25 +2,58 @@
 using namespace std;
 
 int main() {
-int matrix[3][3];
+int matrix1[4][4];
+int matrix2[4][4];
+int sumMatrix[4][4];
 
-  cout << "enter the element of the 3x3 matrix: "<< endl;
-  for(int i= 0; i < 3 ; i++){
-    for (int j = 0; j< 3 ; j++){
+  cout << "enter the element of the 4x4 matrix1"<< endl;
+  for(int i= 0; i < 4 ; i++){
+    for (int j = 0; j< 4 ; j++){
         cout << "element ["<< i <<"]["<< j <<"]: ";
-        cin >> matrix[i][j];
+        cin >> matrix1[i][j];
+    }
+  }
+
+  cout << "enter the element of the 4x4 matrix2: "<< endl;
+  for(int i= 0; i < 4 ; i++){
+    for (int j = 0; j< 4 ; j++){
+        cout << "element ["<< i <<"]["<< j <<"]: ";
+        cin >> matrix2[i][j];
     }
   }
 
 
-cout << "\n the 3x3 matris is: "<< endl;
-    for (int i =0; i< 3 ; i++){
-         for (int j =0; j< 3 ; j++){
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++) {
+            sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
 
 
-        cout << matrix[i][j]<< " ";
+
+cout << "\n the 4x4 matris is: "<< endl;
+    for (int i =0; i< 4 ; i++){
+         for (int j =0; j< 4 ; j++){
+        cout << matrix1[i][j]<< " ";
     }
     cout << endl;
 }
+
+cout << "\n the 4x4 matris is: "<< endl;
+    for (int i =0; i< 4 ; i++){
+         for (int j =0; j< 4 ; j++){
+        cout <<matrix2[i][j]<< " ";
+    }
+    cout << endl;
+}
+
+cout << "\n the 4x4 matris is: "<< endl;
+    for (int i =0; i< 4 ; i++){
+         for (int j =0; j< 4 ; j++){
+        cout << sumMatrix[i][j]<< " ";
+    }
+    cout << endl;
+}
+
 return 0;
 }
